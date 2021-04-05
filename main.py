4078bc209927ee131a7e045602104b28d2fed5d3
Emre sahin <sahinemre@outlook.com>
2021-04-05 15:20:50 +0300

    creating number and button on screen

---
+++ main.py
@@ -0,0 +1,77 @@
+import tkinter as tki
+
+root=tki.Tk()
+
+root.title("Simple Calculator")
+
+e=tki.Entry(root,width=35,borderwidth=5)
+e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
+
+###------FUNCTIONS BEGIN------###
+
+#button add
+def buttonAdd():
+    pass
+
+#button number 
+def buttonClick(number):
+
+    current=e.get()
+    e.delete(0,tki.END)
+    e.insert(0,str(current)+str(number))
+
+#button adding
+def buttonAdd():
+    pass
+
+#button equal
+def buttonEqual():
+    pass
+
+def buttonClear():
+    pass
+
+###------FUNCTION END-------###
+
+#define buttons and put on screen
+button_1=tki.Button(root,text="1",padx=40,pady=20,command=lambda : buttonClick(1))
+button_1.grid(row=1,column=0)
+
+button_2=tki.Button(root,text="2",padx=40,pady=20,command=lambda : buttonClick(2))
+button_2.grid(row=1,column=1)
+
+button_3=tki.Button(root,text="3",padx=40,pady=20,command=lambda : buttonClick(3))
+button_3.grid(row=1,column=2)
+
+button_4=tki.Button(root,text="4",padx=40,pady=20,command=lambda : buttonClick(4))
+button_4.grid(row=2,column=0)
+
+button_5=tki.Button(root,text="5",padx=40,pady=20,command=lambda : buttonClick(5))
+button_5.grid(row=2,column=1)
+
+button_6=tki.Button(root,text="6",padx=40,pady=20,command=lambda : buttonClick(6))
+button_6.grid(row=2,column=2)
+
+button_7=tki.Button(root,text="7",padx=40,pady=20,command=lambda : buttonClick(7))
+button_7.grid(row=1,column=0)
+
+button_8=tki.Button(root,text="8",padx=40,pady=20,command=lambda : buttonClick(8))
+button_8.grid(row=1,column=1)
+
+button_9=tki.Button(root,text="9",padx=40,pady=20,command=lambda : buttonClick(9))
+button_9.grid(row=1,column=2)
+
+button_0=tki.Button(root,text="0",padx=40,pady=20,command=lambda : buttonClick(0))
+button_0.grid(row=4,column=0)
+
+button_add=tki.Button(root,text="+",padx=39,pady=20,command=lambda : buttonAdd())
+button_add.grid(row=5,column=0)
+
+button_equal=tki.Button(root,text="=",padx=91,pady=20,command=lambda : buttonEqual())
+button_equal.grid(row=5,column=1,columnspan=2)
+
+button_clear=tki.Button(root,text="Clear",padx=79,pady=20,command=lambda : buttonClear())
+button_clear.grid(row=4,column=1,columnspan=2)
+
+
+root.mainloop()
